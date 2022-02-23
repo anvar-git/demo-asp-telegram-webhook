d0befecbedc5bf5d32b9a5064428d6a327c30cd3
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-02-23 11:00:16 +0500

    updated

--- Controllers/TelegramApiController.cs
+++ Controllers/TelegramApiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -11,11 +12,13 @@ namespace demo_app2.Controllers
     {
         private readonly TelegramBotClient _telegramBotClient;
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ProtectedLocalStorage _localStorage;
 
-        public TelegramBotController(TelegramBot telegramBot, IHttpClientFactory httpClientFactory)
+        public TelegramBotController(TelegramBot telegramBot, IHttpClientFactory httpClientFactory, ProtectedLocalStorage localStorage)
         {
             _telegramBotClient = telegramBot.GetBot().Result;
             _httpClientFactory = httpClientFactory;
+            _localStorage = localStorage;
         }
 
         [HttpGet]
@@ -31,10 +34,24 @@ namespace demo_app2.Controllers
                 return Ok();
 
             if (update.Message!.Type != MessageType.Text)
-                return Ok(); ;
-
+                return Ok();
+            
             var chatId = update.Message.Chat.Id;
+            
+            var result = await _localStorage.GetAsync<string>("userLastMessage");
+            if (result.Success)
+            {
+                Message sentMessage2 = await _telegramBotClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "Your last value" + result.Value,
+                    parseMode: ParseMode.Markdown
+                );
+            }
+
             var messageText = update.Message.Text;
+
+            await _localStorage.SetAsync("userLastMessage", messageText);
+            
             messageText = messageText ?? "no text";
             Message sentMessage = await _telegramBotClient.SendTextMessageAsync(
                 chatId: chatId,
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using demo_app2;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ builder.Configuration.AddJsonFile("appsettings.local.json", optional: true, relo
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddSingleton<TelegramBot>();
+builder.Services.AddSingleton<ProtectedLocalStorage>();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
